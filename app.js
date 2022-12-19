fac429cd516178a3d99c8282c02619c2201f945f
Chan Zhe Xiang <czhexg1@gmail.com>
2022-12-20 02:46:11 +0800

    Level 6 - Google OAuth 2.0 Authentication

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -29,16 +31,43 @@ mongoose.set("strictQuery", false);
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
+    googleId: String,
+    secret: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, { id: user.id, username: user.username, name: user.name });
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            callbackURL: "http://localhost:3000/auth/google/secrets",
+        },
+        (accessToken, refreshToken, profile, cb) => {
+            User.findOrCreate({ googleId: profile.id }, (err, user) => {
+                return cb(err, user);
+            });
+        }
+    )
+);
 
 const connectDB = async () => {
     try {
@@ -54,17 +83,55 @@ app.route("/").get((req, res) => {
     res.render("home");
 });
 
-app.route("/secrets").get((req, res) => {
-    res.set(
-        "Cache-Control",
-        "no-cache, private, no-store, must-revalidate, max-stal e=0, post-check=0, pre-check=0"
-    );
-    if (req.isAuthenticated()) {
-        res.render("secrets");
-    } else {
-        res.redirect("/login");
+app.route("/auth/google").get(
+    passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.route("/auth/google/secrets").get(
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    (req, res) => {
+        res.redirect("/secrets");
     }
+);
+
+app.route("/secrets").get((req, res) => {
+    User.find({ secret: { $ne: null } }, (err, foundUsers) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets", { usersWithSecret: foundUsers });
+        }
+    });
 });
+
+app.route("/submit")
+    .get((req, res) => {
+        res.set(
+            "Cache-Control",
+            "no-cache, private, no-store, must-revalidate, max-stal e=0, post-check=0, pre-check=0"
+        );
+        if (req.isAuthenticated()) {
+            res.render("submit");
+        } else {
+            res.redirect("/login");
+        }
+    })
+    .post((req, res) => {
+        const submittedSecret = req.body.secret;
+
+        User.findById(req.user.id, (err, foundUser) => {
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundUser) {
+                    foundUser.secret = submittedSecret;
+                    foundUser.save(() => {
+                        res.redirect("/secrets");
+                    });
+                }
+            }
+        });
+    });
 
 app.route("/login")
     .get((req, res) => {
